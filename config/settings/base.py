14b9c5d6e9144795befb5ac5eb38d30c03f2f6f5
maxx <m.petrykin@gmx.de>
2026-04-20 21:39:22 +0200

    static fix + css moved to /static

--- config/settings/base.py
+++ config/settings/base.py
@@ -65,8 +65,12 @@ DATABASES = {
     "default": env.db("DATABASE_URL")
 }
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
