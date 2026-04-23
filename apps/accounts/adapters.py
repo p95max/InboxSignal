8f1916ef8868c95c012ce9a5101f1c582d77531b
maxx <m.petrykin@gmx.de>
2026-04-23 09:51:49 +0200

    remove email confirmation url generation logging

--- apps/accounts/adapters.py
+++ apps/accounts/adapters.py
@@ -1,27 +1,9 @@
-import logging
-
 from allauth.account.adapter import DefaultAccountAdapter
 from django.urls import reverse
 
 
-logger = logging.getLogger(__name__)
-
-
 class AccountAdapter(DefaultAccountAdapter):
     """Custom allauth account adapter."""
-
-    def get_email_confirmation_url(self, request, emailconfirmation):
-        url = super().get_email_confirmation_url(request, emailconfirmation)
-
-        logger.info(
-            "email_confirmation_url_generated",
-            extra={
-                "user_id": emailconfirmation.email_address.user_id,
-                "email_confirmation_generated": True,
-            },
-        )
-
-        return url
 
     def get_email_verification_redirect_url(self, email_address):
         """Redirect users after successful email verification."""
